bb58038187bc3427a02b0bd504280ccc5f6e8abd
DESKTOP-A45213N\Asad <m.asadh@outlook.com>
2023-03-13 14:56:22 -0400

    output generation added

---
+++ sample/generate.py
@@ -0,0 +1,196 @@
+# This code is based on https://github.com/openai/guided-diffusion
+"""
+Generate a large batch of image samples from a model and save them as a large
+numpy array. This can be used to produce samples for FID evaluation.
+"""
+from utils.fixseed import fixseed
+import os
+import numpy as np
+import torch
+from utils.parser_util import generate_args
+from utils import dist_util
+from data_loaders.get_data import get_dataset_loader
+from data_loaders.humanml.scripts.motion_process import recover_from_ric
+import data_loaders.humanml.utils.paramUtil as paramUtil
+from data_loaders.humanml.utils.plot_script import plot_3d_motion
+import shutil
+from data_loaders.tensors import collate
+import test_ddgan as dg
+from score_sde.models.ncsnpp_generator_adagn import NCSNpp
+from model.rotation2xyz import Rotation2xyz
+
+# args: dataset, batch_size, num_samples, output_dir, model_path
+def main():
+    device = 'cuda:0'
+    args = generate_args()
+    fixseed(args.seed)
+    out_path = args.output_dir
+    
+    max_frames = 196 if args.dataset in ['kit', 'humanml'] else 60
+    fps = 12.5 if args.dataset == 'kit' else 20
+    n_frames = min(max_frames, int(args.motion_length*fps))
+    dist_util.setup_dist(device)
+    if out_path == '':
+        out_path = os.path.join(os.path.dirname(args.model_path), 'samples')
+
+    all_motions = []
+    all_lengths = []
+    all_text = []
+    total_num_samples = args.num_samples
+    data_rep = 'hml_vec'
+    njoints = 263
+    nfeats = 1
+
+    data = load_dataset(args, max_frames, n_frames)
+
+    netG = NCSNpp(args).to(device)
+    ckpt = torch.load('./saved_info/dd_gan/{}/{}/netG_{}.pth'.format(args.dataset, args.exp, args.epoch_id), map_location=device)
+    
+    #loading weights from ddp in single gpu
+    for key in list(ckpt.keys()):
+        ckpt[key[7:]] = ckpt.pop(key)
+    netG.load_state_dict(ckpt)
+    netG.eval()
+
+    T = dg.get_time_schedule(args, device)
+    
+    pos_coeff = dg.Posterior_Coefficients(args, device)
+        
+    iters_needed = 50000 // args.batch_size
+    
+    save_dir = "./generated_samples/{}".format(args.dataset)
+    
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    x_t_1 = torch.randn(args.batch_size, args.num_channels,args.image_size, args.image_size).to(device)
+    sample = dg.sample_from_model(pos_coeff, netG, args.num_timesteps, x_t_1,T,  args)
+
+    # Recover XYZ *positions* from HumanML3D vector representation
+    if data_rep == 'hml_vec':
+        n_joints = 22 if sample.shape[1] == 263 else 21
+        sample = data.dataset.t2m_dataset.inv_transform(sample.cpu().permute(0, 2, 3, 1)).float()
+        sample = recover_from_ric(sample, n_joints)
+        sample = sample.view(-1, *sample.shape[2:]).permute(0, 2, 3, 1)
+
+    iterator = iter(data)
+    _, model_kwargs = next(iterator)
+
+    rot2xyz_pose_rep = 'xyz' if data_rep in ['xyz', 'hml_vec'] else data_rep
+    rot2xyz_mask = None if rot2xyz_pose_rep == 'xyz' else model_kwargs['y']['mask'].reshape(args.batch_size, n_frames).bool()
+    rot2xyz = Rotation2xyz(device='cpu', dataset=args.dataset)
+    sample = rot2xyz(x=sample, mask=rot2xyz_mask, pose_rep=rot2xyz_pose_rep, glob=True, translation=True,
+                            jointstype='smpl', vertstrans=True, betas=None, beta=0, glob_rot=None,
+                            get_rotations_back=False)
+
+    all_motions.append(sample.cpu().numpy())
+    all_lengths.append(model_kwargs['y']['lengths'].cpu().numpy())
+
+    print(f"created {len(all_motions) * args.batch_size} samples")
+
+
+    all_motions = np.concatenate(all_motions, axis=0)
+    all_motions = all_motions[:total_num_samples]  # [bs, njoints, 6, seqlen]
+    all_text = all_text[:total_num_samples]
+    all_lengths = np.concatenate(all_lengths, axis=0)[:total_num_samples]
+
+    if os.path.exists(out_path):
+        shutil.rmtree(out_path)
+    os.makedirs(out_path)
+
+    npy_path = os.path.join(out_path, 'results.npy')
+    print(f"saving results file to [{npy_path}]")
+    np.save(npy_path,
+            {'motion': all_motions, 'text': all_text, 'lengths': all_lengths,
+             'num_samples': args.num_samples, 'num_repetitions': args.num_repetitions})
+    with open(npy_path.replace('.npy', '.txt'), 'w') as fw:
+        fw.write('\n'.join(all_text))
+    with open(npy_path.replace('.npy', '_len.txt'), 'w') as fw:
+        fw.write('\n'.join([str(l) for l in all_lengths]))
+
+    print(f"saving visualizations to [{out_path}]...")
+    skeleton = paramUtil.kit_kinematic_chain if args.dataset == 'kit' else paramUtil.t2m_kinematic_chain
+
+    sample_files = []
+    num_samples_in_out_file = 7
+
+    sample_print_template, row_print_template, all_print_template, \
+    sample_file_template, row_file_template, all_file_template = construct_template_variables(args.unconstrained)
+
+    for sample_i in range(args.num_samples):
+        rep_files = []
+        for rep_i in range(args.num_repetitions):
+            caption = all_text[rep_i*args.batch_size + sample_i]
+            length = all_lengths[rep_i*args.batch_size + sample_i]
+            motion = all_motions[rep_i*args.batch_size + sample_i].transpose(2, 0, 1)[:length]
+            save_file = sample_file_template.format(sample_i, rep_i)
+            print(sample_print_template.format(caption, sample_i, rep_i, save_file))
+            animation_save_path = os.path.join(out_path, save_file)
+            plot_3d_motion(animation_save_path, skeleton, motion, dataset=args.dataset, title=caption, fps=fps)
+            # Credit for visualization: https://github.com/EricGuo5513/text-to-motion
+            rep_files.append(animation_save_path)
+
+        sample_files = save_multiple_samples(args, out_path,
+                                               row_print_template, all_print_template, row_file_template, all_file_template,
+                                               caption, num_samples_in_out_file, rep_files, sample_files, sample_i)
+
+    abs_path = os.path.abspath(out_path)
+    print(f'[Done] Results are at [{abs_path}]')
+
+
+def save_multiple_samples(args, out_path, row_print_template, all_print_template, row_file_template, all_file_template,
+                          caption, num_samples_in_out_file, rep_files, sample_files, sample_i):
+    all_rep_save_file = row_file_template.format(sample_i)
+    all_rep_save_path = os.path.join(out_path, all_rep_save_file)
+    ffmpeg_rep_files = [f' -i {f} ' for f in rep_files]
+    hstack_args = f' -filter_complex hstack=inputs={args.num_repetitions}' if args.num_repetitions > 1 else ''
+    ffmpeg_rep_cmd = f'ffmpeg -y -loglevel warning ' + ''.join(ffmpeg_rep_files) + f'{hstack_args} {all_rep_save_path}'
+    os.system(ffmpeg_rep_cmd)
+    print(row_print_template.format(caption, sample_i, all_rep_save_file))
+    sample_files.append(all_rep_save_path)
+    if (sample_i + 1) % num_samples_in_out_file == 0 or sample_i + 1 == args.num_samples:
+        # all_sample_save_file =  f'samples_{(sample_i - len(sample_files) + 1):02d}_to_{sample_i:02d}.mp4'
+        all_sample_save_file = all_file_template.format(sample_i - len(sample_files) + 1, sample_i)
+        all_sample_save_path = os.path.join(out_path, all_sample_save_file)
+        print(all_print_template.format(sample_i - len(sample_files) + 1, sample_i, all_sample_save_file))
+        ffmpeg_rep_files = [f' -i {f} ' for f in sample_files]
+        vstack_args = f' -filter_complex vstack=inputs={len(sample_files)}' if len(sample_files) > 1 else ''
+        ffmpeg_rep_cmd = f'ffmpeg -y -loglevel warning ' + ''.join(
+            ffmpeg_rep_files) + f'{vstack_args} {all_sample_save_path}'
+        os.system(ffmpeg_rep_cmd)
+        sample_files = []
+    return sample_files
+
+
+def construct_template_variables(unconstrained):
+    row_file_template = 'sample{:02d}.mp4'
+    all_file_template = 'samples_{:02d}_to_{:02d}.mp4'
+    if unconstrained:
+        sample_file_template = 'row{:02d}_col{:02d}.mp4'
+        sample_print_template = '[{} row #{:02d} column #{:02d} | -> {}]'
+        row_file_template = row_file_template.replace('sample', 'row')
+        row_print_template = '[{} row #{:02d} | all columns | -> {}]'
+        all_file_template = all_file_template.replace('samples', 'rows')
+        all_print_template = '[rows {:02d} to {:02d} | -> {}]'
+    else:
+        sample_file_template = 'sample{:02d}_rep{:02d}.mp4'
+        sample_print_template = '["{}" ({:02d}) | Rep #{:02d} | -> {}]'
+        row_print_template = '[ "{}" ({:02d}) | all repetitions | -> {}]'
+        all_print_template = '[samples {:02d} to {:02d} | all repetitions | -> {}]'
+
+    return sample_print_template, row_print_template, all_print_template, \
+           sample_file_template, row_file_template, all_file_template
+
+
+def load_dataset(args, max_frames, n_frames):
+    data = get_dataset_loader(name=args.dataset,
+                              batch_size=args.batch_size,
+                              num_frames=max_frames,
+                              split='test',
+                              hml_mode='text_only')
+    data.fixed_length = n_frames
+    return data
+
+
+if __name__ == "__main__":
+    main()
